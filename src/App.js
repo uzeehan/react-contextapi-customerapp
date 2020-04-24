bcd1f36ed76991370411772f5d468c64511e7b2f
Zeehan <zeehanhub@gmail.com>
2020-04-25 04:49:18 +1000

    installed react, sass and bourbon, created a new customers class and displayed a list of customers

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Customers from './components/customer/Customers';
+import './scss/main.scss';
+
+function App() {
+  return (
+    <div className="App container">
+      <Customers />
+    </div>
+  );
+}
+
+export default App;
