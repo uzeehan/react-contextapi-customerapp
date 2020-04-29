11abbe56bb7e421ebd0188be5bd245c7196982d1
Zeehan <zeehanhub@gmail.com>
2020-04-29 14:36:28 +1000

    updated the folder name Consumer to Contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Contacts from './components/customer/Contacts';
+import Contacts from './components/Contact/Contacts';
 import { Provider } from './Context';
 import './scss/main.scss';
 
