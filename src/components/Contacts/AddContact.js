2a4990807490618ea658dd8f2231e9c494413884
Zeehan <zeehanhub@gmail.com>
2020-04-30 16:48:57 +1000

    added onChange event handler for form input fields

--- src/components/Contacts/AddContact.js
+++ src/components/Contacts/AddContact.js
@@ -8,26 +8,43 @@ export class AddContact extends Component {
         phone: ""
     }
 
+    // onNameChange = e => this.setState({ name: e.target.value });
+    // onEmailChange = e => this.setState({ email: e.target.value });
+    // onPhoneChange = e => this.setState({ phone: e.target.value});
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmitAddContact = e => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         const { name, email, phone } = this.state;
         return (
             <div>
                 <h3>ADD CONTACT</h3>
-                <form>
+                <form onSubmit={this.onSubmitAddContact}>
                     <div className="form-group row">
                         <label className="col-md-2" htmlFor="name">NAME</label>
                         <input type="text"
-                        value={name} name="name" className="col-md-10 form-control" placeholder="Enter Name..."/>
+                        value={name}
+                        onChange={this.onChange} name="name" className="col-md-10 form-control" 
+                        placeholder="Enter Name..."/>
                     </div>
                     <div className="form-group row">
                         <label htmlFor="email" className="col-md-2">EMAIL</label>
                         <input type="email"
-                        value={email} className="col-md-10 form-control" name="email" placeholder="Enter Email..."/>
+                        value={email} className="col-md-10 form-control" 
+                        onChange={this.onChange} name="email" 
+                        placeholder="Enter Email..."/>
                     </div>
                     <div className="form-group row">
                         <label htmlFor="phone" className="col-md-2">PHONE</label>
                         <input type="text"
-                        value={phone} name="phone" placeholder="Enter Phone..." className="col-md-10 form-control"/>
+                        value={phone} name="phone" placeholder="Enter Phone..." 
+                        onChange={this.onChange}
+                        className="col-md-10 form-control"/>
                     </div>
                     <div className="form-group row">
                         <input type="submit" value="Add Contact" className="btn btn-warning btn-block"/>
